85ea4484198912c544f60c22a8fbe7a5fa2af083
FranciscoRaffin <francisco.s.raffin@gmail.com>
2024-10-30 23:09:06 -0300

    suma de numertos

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,19 @@
 def promedio(num1, num2):
     return (num1 + num2) / 2
 
+
+def suma5nums(num1, num2, num3, num4, num5):
+    return num1 + num2 + num3 + num4 + num5
+
+
+print("La suma de tun numeros es")
+print(suma5nums(1,2,3,4,5))
+
+
 print("Tu promedio es")
 print(promedio(2,7))
+
+
+
+
+
